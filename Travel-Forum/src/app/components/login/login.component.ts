dff3748eb191d7efa9831f68f17fcedd0762071c
TanyaZhecheva <jechevatanq@gmail.com>
2024-07-22 16:25:03 +0300

    add an login and register component

--- Travel-Forum/src/app/components/login/login.component.ts
+++ Travel-Forum/src/app/components/login/login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login(this.email, this.password);
+    this.email = '';
+    this.password = '';
+  }
+}
