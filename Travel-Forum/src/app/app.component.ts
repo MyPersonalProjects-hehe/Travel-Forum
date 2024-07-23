04fdf42c7f21ab1cf9227f1b11073cc39ec0b4fd
TanyaZhecheva <jechevatanq@gmail.com>
2024-07-23 19:20:52 +0300

    add an ngInit to subscribe to the current user

--- Travel-Forum/src/app/app.component.ts
+++ Travel-Forum/src/app/app.component.ts
@@ -1,25 +1,55 @@
-import { Component, Input, input } from '@angular/core';
-import { Route, Router, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { PopUp } from './components/login/pop-up.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NgIf } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { UserService } from './services/user.service';
+import { User } from 'firebase/auth';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PopUp, LoginComponent, RegisterComponent, NgIf],
+  imports: [
+    RouterOutlet,
+    PopUp,
+    LoginComponent,
+    RegisterComponent,
+    NgIf,
+    HomeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Travel-Forum';
   activeComponent = true;
+  user: User | null = null;
+  isLoggedIn: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private auth: AuthService
+  ) {}
+
   onButtonClicked(value: any) {
     console.log(value.value);
     this.activeComponent = false;
-    this.router.navigate(['register']);
+  }
+
+  ngOnInit() {
+    console.log(this.isLoggedIn);
+
+    this.userService.user$.subscribe((user) => {
+      this.user = user;
+      this.isLoggedIn = !!user;
+    });
+  }
+
+  logout() {
+    this.auth.logout();
   }
 }
