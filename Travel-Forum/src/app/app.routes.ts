050bb504c8c24ae9ab607bcc57974dc752550fb8
TanyaZhecheva <jechevatanq@gmail.com>
2024-07-25 19:30:24 +0300

    add two function guards when the user is logged and not logged

--- Travel-Forum/src/app/app.routes.ts
+++ Travel-Forum/src/app/app.routes.ts
@@ -2,10 +2,23 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { authGuard, notAuthGuard } from './services/auth.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [notAuthGuard],
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [notAuthGuard],
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard],
+  },
   //{ path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
