ed2b01847eacd6cc1e5f4d56160dc6d642258cc7
TanyaZhecheva <jechevatanq@gmail.com>
2024-07-26 23:21:17 +0300

    implement the css and add a listener function for posts

--- Travel-Forum/src/app/components/home/home.component.ts
+++ Travel-Forum/src/app/components/home/home.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { NgFor } from '@angular/common';
+import { onValue, ref } from 'firebase/database';
+import { Database } from '@angular/fire/database';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  currentUser: any = null;
+  posts$: any = null;
+  constructor(
+    private userService: UserService,
+    private auth: AuthService,
+    private db: Database
+  ) {}
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe((user) => (this.currentUser = user));
+    const userRef = ref(this.db, `posts/${this.currentUser?.uid}`);
+
+    onValue(userRef, (snapshot) => {
+      const data = snapshot.val();
+
+      this.posts$ = Object.entries(data).map(([id, post]) => ({
+        id,
+        post,
+      }));
+      console.log('User data updated:', this.posts$);
+    });
+  }
+}
