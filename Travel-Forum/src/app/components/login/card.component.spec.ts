1d88062eba4759a51345eb53020a311f3e879471
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-01 15:06:27 +0300

    rename the pop-up component to card component

--- Travel-Forum/src/app/components/login/card.component.spec.ts
+++ Travel-Forum/src/app/components/login/card.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PopUp } from './pop-up.component';
+import { CardComponent } from './card.component';
 
-describe('PopUp', () => {
-  let component: PopUp;
-  let fixture: ComponentFixture<PopUp>;
+describe('CardComponent', () => {
+  let component: CardComponent;
+  let fixture: ComponentFixture<CardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PopUp],
+      imports: [CardComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(PopUp);
+    fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
