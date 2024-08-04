1c89e0db05b274e2bc1f81c2dfd237dfdb3bda97
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-04 15:14:53 +0300

    add a like component

---
+++ Travel-Forum/src/app/icons/like/like.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'like',
+  standalone: true,
+  imports: [],
+  templateUrl: './like.component.html',
+  styleUrl: './like.component.css',
+})
+export class LikeComponent {}
