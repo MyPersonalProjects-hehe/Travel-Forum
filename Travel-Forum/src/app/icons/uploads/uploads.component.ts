f5e85768ca5020f7e047f08f4e6aba4bd99a5fd3
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-04 14:08:03 +0300

    add an uploading svg component

---
+++ Travel-Forum/src/app/icons/uploads/uploads.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'uploads',
+  standalone: true,
+  imports: [],
+  templateUrl: './uploads.component.html',
+  styleUrl: './uploads.component.css',
+})
+export class UploadsComponent {}
