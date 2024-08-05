918265b3c0a2b694ec56f7421500ecbe3b941362
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-05 15:34:10 +0300

    add a dislike component

---
+++ Travel-Forum/src/app/icons/like/dislike/dislike.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dislike',
+  standalone: true,
+  imports: [],
+  templateUrl: './dislike.component.html',
+  styleUrl: './dislike.component.css',
+})
+export class DislikeComponent {}
