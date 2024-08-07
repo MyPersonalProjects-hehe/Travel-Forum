9580db0837130354313f530c3a16512bae609c18
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-07 16:48:30 +0300

    add comment section

--- Travel-Forum/src/app/components/posts/full-view-post/full.view.post.component.ts
+++ Travel-Forum/src/app/components/posts/full-view-post/full.view.post.component.ts
@@ -3,11 +3,14 @@ import { PostComponent } from '../post/post.component';
 import { AuthService } from '../../../services/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../../../services/user.service';
+import { onValue, ref } from 'firebase/database';
+import { Database } from '@angular/fire/database';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'fullViewPost',
   standalone: true,
-  imports: [PostComponent],
+  imports: [PostComponent, NgFor, NgIf],
   templateUrl: './full.view.post.component.html',
   styleUrl: './full.view.post.component.css',
 })
@@ -15,16 +18,35 @@ export class FullViewPostComponent implements OnInit {
   currentUser: any;
   post$: any;
   postId: any;
+  isCommentSectionOpen: boolean = false;
+  comments$: any;
+  userId: any;
 
   constructor(
     private auth: AuthService,
     private route: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private db: Database
   ) {}
 
   async ngOnInit() {
     this.auth.user$.subscribe((user) => (this.currentUser = user));
+    this.userId = this.currentUser.uid;
     this.postId = this.route.snapshot.paramMap.get('id');
     this.post$ = await this.userService.fetchPost(this.postId);
+
+    const postsRef = ref(this.db, `posts/${this.post$.id}/comments`);
+
+    onValue(postsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        this.comments$ = Object.values(data);
+        console.log(this.comments$, this.userId);
+      }
+    });
+  }
+
+  toggleCommentSection() {
+    this.isCommentSectionOpen = !this.isCommentSectionOpen;
   }
 }
