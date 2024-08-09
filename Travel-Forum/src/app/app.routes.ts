c71a58542f1fd31a09059dace0e1b1c4b8a7626f
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-09 20:07:27 +0300

    rename the components because of changes in the file naming

--- Travel-Forum/src/app/app.routes.ts
+++ Travel-Forum/src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { authGuard, notAuthGuard } from './services/auth.guard';
 import { CreatePost } from './components/posts/create.post/create.post.component';
-import { AllPostsComponent } from './components/posts/all-posts/all.posts.component';
-import { FullViewPostComponent } from './components/posts/full-view-post/full.view.post.component';
+import { FullViewPostComponent } from './components/posts/full.view.post/full.view.post.component';
+import { AllPostsComponent } from './components/posts/all.posts/all.posts.component';
 
 export const routes: Routes = [
   {
