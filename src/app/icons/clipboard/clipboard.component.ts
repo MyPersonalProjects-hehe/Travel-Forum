dc3efa2de92596faba0b92f94cd6989d1c0bff43
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-17 13:45:33 +0300

    add a clipboard svg icon

---
+++ src/app/icons/clipboard/clipboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clipboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './clipboard.component.html',
+  styleUrl: './clipboard.component.scss',
+})
+export class ClipboardComponent {}
