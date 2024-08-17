2750ad4f78e3422ec3eef296fa7568bb2719e769
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-17 13:45:27 +0300

    add a camera svg icon

---
+++ src/app/icons/camera/camera.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'camera',
+  standalone: true,
+  imports: [],
+  templateUrl: './camera.component.html',
+  styleUrl: './camera.component.scss',
+})
+export class CameraComponent {}
