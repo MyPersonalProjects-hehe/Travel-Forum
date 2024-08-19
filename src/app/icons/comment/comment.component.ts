b096a528879b92a05b017e4a3010752f1417d8e7
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-19 23:13:31 +0300

    move the comments length functionality to its component

--- src/app/icons/comment/comment.component.ts
+++ src/app/icons/comment/comment.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'comment',
@@ -8,6 +8,20 @@ import { Component, Input } from '@angular/core';
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss',
 })
-export class CommentComponent {
-  @Input() commentsCount: any;
+export class CommentComponent implements OnInit {
+  @Input() post: any;
+  countOfComments: any;
+
+  ngOnInit() {
+    const commentsCount: any = [this.post?.post].filter(
+      (post: any) => post?.comments
+    );
+
+    if (commentsCount.length > 0) {
+      this.countOfComments = commentsCount[0];
+      this.countOfComments = Object.values(
+        this.countOfComments?.comments
+      ).length;
+    }
+  }
 }
