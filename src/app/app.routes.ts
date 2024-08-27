54f0ccc713fdf13865608a9f988b682e20a60a86
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-27 17:28:41 +0300

    implement  routing by firebase built-in guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,55 +1,68 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
-import { authGuard, notAuthGuard } from './services/auth.guard';
 import { CreatePost } from './components/posts/create.post/create.post.component';
 import { FullViewPostComponent } from './components/posts/full.view.post/full.view.post.component';
 import { AllPostsComponent } from './components/posts/all.posts/all.posts.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { AppComponent } from './app.component';
+import {
+  AuthGuard,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
+import { HomeUnauthenticatedComponent } from './components/home-unauthenticated/home-unauthenticated.component';
+
+const redirectUnauthorizedToLogin = () =>
+  redirectUnauthorizedTo(['/home-unauthenticated']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['/home']);
 
 export const routes: Routes = [
   {
-    path: 'login',
-    component: LoginComponent,
-    canActivate: [notAuthGuard],
+    path: 'home-unauthenticated',
+    component: HomeUnauthenticatedComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectLoggedInToHome },
   },
-  {
-    path: 'register',
-    component: RegisterComponent,
-    canActivate: [notAuthGuard],
-  },
+
   {
     path: 'home',
     component: HomeComponent,
-    canActivate: [authGuard],
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: 'createPost',
     component: CreatePost,
-    canActivate: [authGuard],
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: 'allPosts',
     component: AllPostsComponent,
-    canActivate: [authGuard],
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: 'fullViewPost/:id',
     component: FullViewPostComponent,
-    canActivate: [authGuard],
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: 'about',
     component: AboutComponent,
-    canActivate: [authGuard],
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: 'profile/:id',
     component: ProfileComponent,
-    canActivate: [authGuard],
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
-  { path: '', component: HomeComponent, canActivate: [authGuard] },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
 ];
