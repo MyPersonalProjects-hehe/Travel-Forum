babddbd7f5047183e3b66cdb7fce112a898ce926
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-27 12:51:51 +0300

    delete svg icons with no functionality

--- src/app/components/about/info.byte/info.byte.component.ts
+++ src/app/components/about/info.byte/info.byte.component.ts
@@ -1,13 +1,10 @@
 import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { CommentComponent } from '../../../icons/comment/comment.component';
-import { ClipboardComponent } from '../../../icons/clipboard/clipboard.component';
-import { CameraComponent } from '../../../icons/camera/camera.component';
 
 @Component({
   selector: 'infoByte',
   standalone: true,
-  imports: [NgIf, CommentComponent, ClipboardComponent, CameraComponent],
+  imports: [NgIf],
   templateUrl: './info.byte.component.html',
   styleUrl: './info.byte.component.scss',
 })
