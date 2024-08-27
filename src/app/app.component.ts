a44cb75e11ef5997451ab9691b18e8bd5f580974
TanyaZhecheva <jechevatanq@gmail.com>
2024-08-27 17:30:50 +0300

    import component home-unauthenticated because of rendering two times login/register component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RouterOutlet } from '@angular/router';
 import { NgIf } from '@angular/common';
-import { HomeComponent } from './components/home/home.component';
 import { User } from 'firebase/auth';
 import { AuthService } from './services/auth.service';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -12,28 +9,14 @@ import { CardComponent } from './components/login/card.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    LoginComponent,
-    RegisterComponent,
-    NgIf,
-    HomeComponent,
-    CardComponent,
-    NavbarComponent,
-  ],
+  imports: [RouterOutlet, NgIf, CardComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'Travel-Forum';
-  activeComponent: any = 'Login';
   user: User | null = null;
 
   constructor(private auth: AuthService) {}
-
-  onButtonClicked(value: any) {
-    this.activeComponent = value;
-  }
 
   ngOnInit() {
     this.auth.user$?.subscribe((user) => {
