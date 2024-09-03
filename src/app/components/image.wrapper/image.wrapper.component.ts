6b545cfec0aef2eee2a79f094a7b4edf97474d73
TanyaZhecheva <jechevatanq@gmail.com>
2024-09-03 23:17:17 +0300

    add a conditional rendering for image wrapper because i will reuse it in the noPostsComponent

--- src/app/components/image.wrapper/image.wrapper.component.ts
+++ src/app/components/image.wrapper/image.wrapper.component.ts
@@ -1,13 +1,15 @@
+import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'image-wrapper',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './image.wrapper.component.html',
   styleUrl: './image.wrapper.component.scss',
 })
 export class ImageWrapper {
   @Input() heading: string = '';
   @Input() image: string = '';
+  @Input() isNoPostsComponent: boolean = false;
 }
