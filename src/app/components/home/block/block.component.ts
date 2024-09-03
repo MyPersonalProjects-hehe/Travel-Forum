ee2cb32d604e233c49ad9a5a440951763e3f0da6
TanyaZhecheva <jechevatanq@gmail.com>
2024-09-03 16:12:09 +0300

    move the functionality for fetching likes, comments and etc in a ngOnChanges to detect every change

--- src/app/components/home/block/block.component.ts
+++ src/app/components/home/block/block.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { NgIf } from '@angular/common';
 
 @Component({
@@ -8,6 +8,33 @@ import { NgIf } from '@angular/common';
   templateUrl: './block.component.html',
   styleUrl: './block.component.scss',
 })
-export class BlockComponent {
-  @Input() info: string[] = [];
+export class BlockComponent implements OnChanges {
+  @Input() dateOfRegister: string = '';
+  @Input() posts$: any;
+  @Input() comments: number = 0;
+  @Input() likes: number = 0;
+  @Input() isFirstBlock: boolean = false;
+  userInfo: string[] = [];
+  postsInfo: string[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (
+      changes['comments'] ||
+      changes['dateOfRegister'] ||
+      changes['posts$'] ||
+      changes['isFirstBlock'] ||
+      changes['likes']
+    ) {
+      this.userInfo = [
+        `Your profile info`,
+        `Date of register: ${this.dateOfRegister || 0}`,
+        `Number of uploaded post: ${this.posts$?.length || 0}`,
+      ];
+      this.postsInfo = [
+        `Statistics for posts`,
+        `Comments: ${this.comments || 0} `,
+        `Likes: ${this.likes || 0}`,
+      ];
+    }
+  }
 }
